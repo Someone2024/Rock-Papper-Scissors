c936826299679318dae79d0f243e92d29dd5269b
Someone2024 <bassilsantiago@gmail.com>
2023-05-06 10:00:03 -0500

    for the UI i've added new containers for the buttons and an array of choices plus it's images

--- scripts/UI.js
+++ scripts/UI.js
@@ -1,32 +1,62 @@
+const buttonsContainer = document.getElementById("buttons-container")
+
+const PLAYERCHOICES = ["rock", "papper", "scissors"];
+
 const rockBtn = document.createElement("button");
 const papperBtn = document.createElement("button");
 const scissorsBtn = document.createElement("button");
 
+const rockImg = document.createElement("img");
+const papperImg = document.createElement("img");
+const scissorsImg = document.createElement("img");
+
+rockImg.src = "../assets/rock.png"
+papperImg.src = "../assets/paper.png"
+scissorsImg.src = "../assets/scissors.png"
+
 const GameResult = document.createElement("div");
+GameResult.classList.add("game-result");
 
 const playerScoreP = document.createElement("p");
 const computerScoreP = document.createElement("p");
+const ScoreDiv = document.createElement("div");
+ScoreDiv.classList.add("score-div");
+playerScoreP.classList.add("score");
+computerScoreP.classList.add("score");
 
-const rock = (rockBtn.textContent = "rock");
-papperBtn.textContent = "papper";
-scissorsBtn.textContent = "scissors";
+rockBtn.classList.add("button");
+papperBtn.classList.add("button");
+scissorsBtn.classList.add("button");
+
+rockImg.classList.add("image");
+papperImg.classList.add("image");
+scissorsImg.classList.add("image");
 
 rockBtn.addEventListener("click", () => {
-  GameResult.textContent = game(rockBtn.textContent);
+  GameResult.textContent = game(PLAYERCHOICES[0]);
   playerScoreP.textContent = `Player Score: ${playerScore}`;
-  computerScoreP.textContent = `Computer Score Score: ${computerScore}`;
+  computerScoreP.textContent = `Computer Score: ${computerScore}`;
 });
 
 papperBtn.addEventListener("click", () => {
-  GameResult.textContent = game(papperBtn.textContent);
+  GameResult.textContent = game(PLAYERCHOICES[1]);
   playerScoreP.textContent = `Player Score: ${playerScore}`;
-  computerScoreP.textContent = `Computer Score Score: ${computerScore}`;
+  computerScoreP.textContent = `Computer Score: ${computerScore}`;
 });
 
 scissorsBtn.addEventListener("click", () => {
-  GameResult.textContent = game(scissorsBtn.textContent);
+  GameResult.textContent = game(PLAYERCHOICES[2]);
   playerScoreP.textContent = `Player Score: ${playerScore}`;
-  computerScoreP.textContent = `Computer Score Score: ${computerScore}`;
+  computerScoreP.textContent = `Computer Score: ${computerScore}`;
 });
-document.body.append(rockBtn, papperBtn, scissorsBtn);
-document.body.append(GameResult, playerScoreP, computerScoreP);
+
+rockBtn.appendChild(rockImg);
+papperBtn.appendChild(papperImg);
+scissorsBtn.appendChild(scissorsImg);
+
+buttonsContainer.append(rockBtn, papperBtn, scissorsBtn);
+document.body.append(buttonsContainer);
+
+ScoreDiv.append(playerScoreP, computerScoreP);
+
+document.body.append(ScoreDiv, GameResult);
